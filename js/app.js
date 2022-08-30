6c4262dd1c96c705b09675fe59e23ad3476d2221
martincavigliasso <martincavigliasso@gmail.com>
2022-08-30 16:42:37 -0300

    proyecto

---
+++ js/app.js
@@ -0,0 +1,81 @@
+
+
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const buscador = document.getElementById('search')
+
+
+buscador.addEventListener('input', ()=>{
+    mostrarProductos(stockProductos.filter(item=> item.nombre.toUpperCase().includes( buscador.value.toUpperCase())))
+})
+
+mostrarProductos(stockProductos)
+
+function mostrarProductos(array){
+    contenedorProductos.innerHTML= ""
+    
+   array.forEach(item => {
+
+       let div = document.createElement('div')
+       div.classList.add('producto')
+    div.innerHTML += `
+                    <div class="card" >
+                        <div class="card-image">
+                            <img src=${item.img}>
+                            <span class="card-title" id="${item.id}">${item.nombre}</span>
+                            <a  id="agregar${item.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>${item.desc}</p>
+                            
+                            <p> $${item.precio}</p>
+                        </div>
+                    </div>
+    `
+    contenedorProductos.appendChild(div)
+
+        let nombreDetalle = document.getElementById(`${item.id}`)
+        nombreDetalle.addEventListener('click',()=>{
+            let guardarLS = stockProductos.find(elemento => elemento.id == item.id)
+            localStorage.setItem('producto', JSON.stringify(guardarLS))
+            
+        })
+
+        let btnAgregar = document.getElementById(`agregar${item.id}`)
+        btnAgregar.addEventListener('click',()=>{
+           agregarAlCarrito(item.id)
+        })
+
+   })
+}
+
+const sa = ()=> {
+    let timerInterval
+    Swal.fire({
+      title: 'OFERTAS QUE VUELAN, APROVECHALAS',
+      html: 'ESTE AVISO SE CERRARA EN <b></b> milliseconds.',
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft()
+        }, 100)
+      },
+      willClose: () => {
+        clearInterval(timerInterval)
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+      }
+    })
+}
+   
+
+
+
+
+
---
+++ js/stock.js
@@ -0,0 +1,24 @@
+let stockProductos = [
+    {id: 1, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 2, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 3, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 4, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 5, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 6, nombre: "Mayonesas", tipo: "mayo", desc: "mayonesa de distintas marcas", precio: 1200, img: '/img/descarga.jpg'},
+    {id: 7, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 8, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 9, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 10, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 11, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 12, nombre: "Aceitunas", tipo: "Aceitunas", desc: "Aceitunas Caseras", precio: 900, img: '/img/nutricion_368975379_112460234_1024x576.jpg'},
+    {id: 13, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 14, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 15, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 16, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 17, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 18, nombre: "Cervezas", tipo: "Birras", desc: "Todas las marcas", precio: 1500, img: '/img/cocinillas_87251509_116232453_1706x960.jpg'},
+    {id: 18, nombre: "Aceites", tipo: "Aceites", desc: "Todas las marcas", precio: 1100, img: '/img/uso-aceites_dec37d7a_1200x1200.jpg'},
+]
+
+
+
